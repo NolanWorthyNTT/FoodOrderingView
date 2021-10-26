5b3fa65346390af5e485f6d6a9bc555b07ef2dee
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-26 18:34:04 -0400

    create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <h3>Footer</h3>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
