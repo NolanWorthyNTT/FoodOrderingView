02dd9bda2e8fe7e77cf3e75aa07842b9543fa43a
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-27 15:23:59 -0400

    update displayed total when cart in React state is updated

--- src/components/CartAsideItem.js
+++ src/components/CartAsideItem.js
@@ -1,23 +1,22 @@
-import { useState, useEffect } from 'react';
 
 function CartAsideItem(props) {
-    const [cartQty, setCartQty] = useState(props.qtyToBuy);
 
-    useEffect(() => {
-        var existingIndex = props.cart.findIndex((o) => o.dishId === props.dishId);
-        setCartQty(props.cart[existingIndex].qtyToBuy);
-    }, [props.cart, props.dishId]);
-    
     const onMinusClick = () => {
-        if(cartQty > 1) {
-            setCartQty(cartQty-1);
+        var existingIndex = props.cart.findIndex((o) => o.dishId === props.dishId);
+        var localCart = props.cart.slice();
+        if(localCart[existingIndex].qtyToBuy > 1) {
+            localCart[existingIndex].qtyToBuy -= 1;
+            props.setCart(localCart);
         }
     }
 
     const onPlusClick = () => {
+        var existingIndex = props.cart.findIndex((o) => o.dishId === props.dishId);
+        var localCart = props.cart.slice();
         // 32767 is max value of SQL SMALLINT - the type of qtyAvailable in Menu table
-        if(cartQty < 32767) {
-            setCartQty(cartQty+1);
+        if(localCart[existingIndex].qtyToBuy < 32767) {
+            localCart[existingIndex].qtyToBuy += 1;
+            props.setCart(localCart);
         }
     }
 
@@ -28,7 +27,7 @@ function CartAsideItem(props) {
                 <div><h5>{props.dishName} - ${props.pricePer}</h5></div>
                 <div><p>{props.ingredients}</p></div>
                 <button type="button" className="cart-aside-item-minus-btn" onClick={onMinusClick}>-</button>
-                {cartQty}
+                {props.cart[props.cart.findIndex((o) => o.dishId === props.dishId)].qtyToBuy}
                 <button type="button" className="cart-aside-item-plus-btn" onClick={onPlusClick}>+</button>
             </div>
         </div>
