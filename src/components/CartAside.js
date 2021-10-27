4486b2749d86163e773b26c341ece7f58a53fbf7
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-27 15:25:06 -0400

    update displayed total when cart in React state is updated

--- src/components/CartAside.js
+++ src/components/CartAside.js
@@ -1,8 +1,22 @@
+import { useState, useEffect } from 'react';
+//import { useHistory } from 'react-router-dom';
 import CartAsideItem from "./CartAsideItem.js";
 
-function cartAside(props) {
-    const onClick = () => {
+function CartAside(props) {
+    //const history = useHistory();
+    const [total, setTotal] = useState(0);
 
+    // whenever the cart is updated, calculate the new total
+    useEffect(() => {
+        setTotal(0);
+        props.cart.forEach((i) => {
+            /// 1.0725 == 7.25% sales tax in Charlotte
+            setTotal(total => total + ((i.pricePer * i.qtyToBuy) * 1.0725));
+        });
+    }, [props.cart]);
+
+    const onClick = () => {
+        
     }
 
     return (
@@ -21,10 +35,10 @@ function cartAside(props) {
                                 setCart={props.setCart}
                     />
                 ))}
-                <button type="button" className="cart-aside-proceed-btn" onClick={onClick}>Proceed To Checkout</button>
+                Total: ${total.toFixed(2)} <button type="button" className="cart-aside-proceed-btn" onClick={onClick}>Proceed To Checkout</button>
             </div>
         </div>
     )
 }
 
-export default cartAside;
+export default CartAside;
