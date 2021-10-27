43a7097316455a6e80d626389d70f62322e360a6
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-26 23:11:51 -0400

    add router to Admin component. pass more props to Header component.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Login from './components/Login.js';
 import Menu from './components/Menu.js';
+import Admin from './components/Admin.js';
 
 function App() {
   const [role, setRole] = useState('');
@@ -13,10 +14,14 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header role={role} username={username} />
+        <Header role={role} setRole={setRole} username={username} setUsername={setUsername} />
         <Switch>
           <Route exact path='/'>
             <Login role={role} setRole={setRole} username={username} setUsername={setUsername} />
+          </Route>
+
+          <Route path='/admin'>
+            <Admin />
           </Route>
 
           <Route path='/menu'>
