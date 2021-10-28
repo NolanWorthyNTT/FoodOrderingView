938efacf29c6df391c2a4b48a481f023acdedf9e
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-27 21:18:20 -0400

    when order is placed, send POST request to API containing order info, clear cart state, and redirect to orders route

--- src/components/CartAside.js
+++ src/components/CartAside.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
-//import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import CartAsideItem from "./CartAsideItem.js";
 
 function CartAside(props) {
-    //const history = useHistory();
+    const history = useHistory();
     const [total, setTotal] = useState(0);
 
     // whenever the cart is updated, calculate the new total
@@ -11,12 +11,39 @@ function CartAside(props) {
         setTotal(0);
         props.cart.forEach((i) => {
             /// 1.0725 == 7.25% sales tax in Charlotte
-            setTotal(total => total + ((i.pricePer * i.qtyToBuy) * 1.0725));
+            setTotal(total => total + ((i.pricePer * i.qty) * 1.0725));
         });
     }, [props.cart]);
 
+    const sendHttpRequest = (method, url, data) => {
+        const promise = new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+
+            xhr.setRequestHeader('Content-type', 'application/json');
+
+            // fires on response
+            xhr.onload = () => {
+                resolve();
+            };
+
+            xhr.send(JSON.stringify(data));
+        })
+        return promise;
+    }
+
     const onClick = () => {
-        
+        sendHttpRequest('POST', 'http://localhost:8080/placeOrder', {
+            "order": {
+                "total": total,
+                "userId": props.userId
+            },
+            "dishesInOrder": props.cart
+        }).then(responseData => {
+            props.setCart([]);
+
+            history.push('/orders');
+        });
     }
 
     return (
