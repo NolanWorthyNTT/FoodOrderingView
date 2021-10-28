3fb0d26e4577f0b887809d1dff3fcdabaac389a4
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-28 10:22:36 -0400

    send GET request to API and get back current user orders. pass order info as props to OrderItem component. one OrderItem component for every user order.

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,9 +1,41 @@
+import { useState, useEffect } from 'react';
+import OrderItem from './OrderItem.js';
 
+function Orders(props) {
+    const [orders, setOrders] = useState([]);
 
-function Orders() {
+    useEffect(() => {
+        sendHttpRequest('GET', 'http://localhost:8080/orders?id=' + props.userId).then(responseData => {
+            setOrders(responseData);
+        });
+    }, [props.userId]);
+
+    const sendHttpRequest = (method, url) => {
+        const promise = new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+
+            // fires on response
+            xhr.onload = () => {
+                resolve(JSON.parse(xhr.response));
+            };
+
+            xhr.send();
+        })
+        return promise;
+    }
+
     return (
         <div>
-            
+            <h2>My Orders</h2>
+            {orders.map((order) => (
+                    <OrderItem key={order.orderId}
+                                orderId={order.orderId}
+                                dateOfOrder={order.dateOfOrder}
+                                total={order.total}
+                                userId={order.userId}
+                    />
+                ))}
         </div>
     )
 }
