831baffbd32d868cfb275cee0a9e0626a21112ea
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-28 16:50:10 -0400

    on load, get dishes in Menu table from API and store in adminMenu state. for every dish in adminMenu, render an AdminMenuItem component. render an Add Dish button that adds an empty dish to adminMenu.

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,9 +1,57 @@
-
+import { useState, useEffect } from 'react';
+import AdminMenuItem from './AdminMenuItem.js';
 
 function Admin() {
+    const [adminMenu, setAdminMenu] = useState([]);
+
+    const getMenu = () => {
+        const promise = new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://localhost:8080/menu");
+
+            // fires on response
+            xhr.onload = () => {
+                resolve(JSON.parse(xhr.response));
+            };
+
+            xhr.send();
+        })
+        return promise;
+    }
+
+    useEffect(() => {
+        getMenu().then(responseData => {
+            setAdminMenu(responseData);
+        });
+    }, []);
+
+    const addDish = (e) => {
+        setAdminMenu([...adminMenu, {}]);
+    }
+
     return (
         <div>
-            <h1>hey</h1>
+            <div>
+                <h3>Current Menu</h3>
+                <div className="admin-menu">
+                    {adminMenu.map((dish) => (
+                        <AdminMenuItem key={dish.dishId}
+                                    dishId={dish.dishId}
+                                    dishName={dish.dishName}
+                                    qty={dish.qty}
+                                    pricePer={dish.pricePer}
+                                    imageUrl={dish.imageUrl}
+                                    ingredients={dish.ingredients}
+                                    adminMenu={adminMenu}
+                                    setAdminMenu={setAdminMenu}
+                        />
+                    ))}
+                </div>
+                    <button onClick={addDish}>Add Dish</button>
+            </div>
+            <div>
+                <h3>All past dishes</h3>
+            </div>
         </div>
     );
 }
