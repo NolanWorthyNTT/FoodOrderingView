cfbef2c3dbb9d06a16c210e868dfe1eaa04b5656
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-29 08:47:23 -0400

    on page load, make GET request to API /dishes endpoint and store response in dishes state. for every item in dishes that isn't in adminMenu, render a PastDishItem component.

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,13 +1,30 @@
 import { useState, useEffect } from 'react';
 import AdminMenuItem from './AdminMenuItem.js';
+import PastDishItem from './PastDishItem.js';
 
 function Admin() {
     const [adminMenu, setAdminMenu] = useState([]);
+    const [dishes, setDishes] = useState([]);
 
     const getMenu = () => {
         const promise = new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest();
             xhr.open("GET", "http://localhost:8080/menu");
+
+            // fires on response
+            xhr.onload = () => {
+                resolve(JSON.parse(xhr.response));
+            };
+
+            xhr.send();
+        })
+        return promise;
+    }
+
+    const getDishes = () => {
+        const promise = new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://localhost:8080/dishes");
 
             // fires on response
             xhr.onload = () => {
@@ -28,21 +45,26 @@ function Admin() {
 
             // fires on response
             xhr.onload = () => {
-                resolve(JSON.parse(xhr.response));
+                resolve();
             };
 
             xhr.send(JSON.stringify(requestData));
         })
         return promise;
     }
+
     useEffect(() => {
         getMenu().then(responseData => {
             setAdminMenu(responseData);
         });
+
+        getDishes().then(responseData => {
+            setDishes(responseData);
+        });
     }, []);
 
     const addDish = (e) => {
-        setAdminMenu([...adminMenu, {dishId: -1, dishName: "", qty: null, pricePer: null, imageUrl: "", ingredients: ""}]);
+        setAdminMenu([...adminMenu, {dishId: -1, dishName: "", qty: 0, pricePer: 0, imageUrl: "", ingredients: ""}]);
     }
     
     const submitMenu = (e) => {
@@ -79,14 +101,33 @@ function Admin() {
                                     ingredients={dish.ingredients}
                                     adminMenu={adminMenu}
                                     setAdminMenu={setAdminMenu}
+                                    dishes={dishes}
+                                    setDishes={setDishes}
                         />
                     ))}
                 </div>
                     <button onClick={addDish}>Add Dish</button>
                     <button onClick={submitMenu}>Submit Menu</button>
             </div>
-            <div>
+            <div className="all-past-dishes">
                 <h3>All Past Dishes</h3>
+                <div className="past-dishes-list">
+                    {/* only show dishes in All Past Dishes that aren't in menu */}
+                    {dishes.filter((dish) => !adminMenu.some(i => i.dishId === dish.dishId))
+                        .map((dish) => (
+                            <PastDishItem key={dish.dishId}
+                                            dishId={dish.dishId}
+                                            dishName={dish.dishName}
+                                            pricePer={dish.pricePer}                
+                                            imageUrl={dish.imageUrl}
+                                            ingredients={dish.ingredients}
+                                            adminMenu={adminMenu}
+                                            setAdminMenu={setAdminMenu}
+                                            dishes={dishes}
+                                            setDishes={setDishes}
+                            />
+                    ))}
+                </div>
             </div>
         </div>
     );
